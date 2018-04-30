dbf2e1ebd0f087bd64a03c580409d1f1dacd1eed
hounslowcodeclub <hounslowcodeclub@gmail.com>
2018-04-30 22:30:21 +0100

    Update return-env.js

--- functions/return-env.js
+++ functions/return-env.js
@@ -2,8 +2,8 @@ exports.handler = function(event, context, callback) {
   // Currently this only works with variables that are defined in the UI and not in the netlify.toml
   // Using ES6 destructuring to pull the variable called FAKE_TOKEN out of the process.env object
   // You can then use the FAKE_TOKEN anywhere in your code just as you would any string variable
-  const { FAKE_TOKEN } = process.env;
-  console.log(FAKE_TOKEN);
+  const { SLACK_WEBHOOK_URL } = process.env;
+  console.log(SLACK_WEBHOOK_URL);
   // Amazon callback function without any custom headers
   callback(null, {
     statusCode: 200,
